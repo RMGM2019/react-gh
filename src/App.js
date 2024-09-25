c61f7d0f7477b9f6832525c865afa81553a013a1
RMGM2019 <48869312+RMGM2019@users.noreply.github.com>
2024-09-24 19:55:54 -0600

    deploy to EB 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          this will deploy automatically on EB
+          this will deploy automatically on EB 2
         </p>
         <a
           className="App-link"
